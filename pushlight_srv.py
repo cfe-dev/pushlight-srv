ebc519b1a0a18ad87fcf4b212a2ce1a916504d89
cfe-dev <subcore@icb.at>
2022-11-19 21:49:24 +0100

    packaging; test data template;

--- pushlight_srv.py
+++ pushlight_srv.py
@@ -5,19 +5,23 @@
 
 import uvicorn
 
-from fastapi import Depends, FastAPI, Request
+from fastapi import APIRouter, Depends, FastAPI, Request
 # from fastapi import HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from utils import crud, models, schemas
+from utils.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
+# app = FastAPI(root_path='/pushlight_srv/v1')
+# app = FastAPI(root_path='/pushlight_srv')
 app = FastAPI()
+
+prefix_router = APIRouter(prefix="/pushlight_srv/v1")
 
 
 # Dependency
@@ -34,25 +38,35 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=RedirectResponse)
+@prefix_router.get("/", response_class=RedirectResponse)
 def root():
-    """get root html; redirect to /items/1/ template test"""
-    return RedirectResponse("/items/1/")
+    """get root html; redirect to items/1 template test"""
+    return RedirectResponse("items/1")
 
 
-@app.get("/items/{item_id}", response_class=HTMLResponse)
+@prefix_router.get("/items/{item_id}", response_class=HTMLResponse)
 async def read_item(request: Request, item_id: str):
     """get item html for specific id"""
     return templates.TemplateResponse("template_item.html",
                                       {"request": request, "item_id": item_id})
 
 
-@app.post("/collect")
+@prefix_router.get("/items/last/{item_count}", response_class=HTMLResponse)
+async def read_lastitems(request: Request, item_count: int, dbconn: Session = Depends(get_db)):
+    """get item html for specific id"""
+    lastitems = crud.get_lastgpsdata(dbconn=dbconn, item_count=item_count)
+    return templates.TemplateResponse("template_gpsdata.html",
+                                      {"request": request, "lastitems": lastitems})
+
+
+@prefix_router.post("/collect")
 async def collect(pushlightdata: schemas.PushLightData, dbconn: Session = Depends(get_db)):
     """append GPS data to persistent storage"""
     print(pushlightdata.json())
     crud.create_gpsdata(dbconn=dbconn, pushlightdata=pushlightdata)
 
+
+app.include_router(prefix_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8220)
--- utils/crud.py
+++ utils/crud.py
@@ -1,13 +1,25 @@
 """database access methods"""
 
+from typing import List
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
 
-def get_gpsdata(dbconn: Session, data_id: int):
+def get_gpsdata(dbconn: Session, data_id: int) -> models.GpsData:
     """return a single data point by id"""
-    return dbconn.query(models.GpsData).filter(models.GpsData.data_id == data_id).first()
+    return dbconn.query(models.GpsData).filter(
+        models.GpsData.data_id == data_id).first()
+
+
+def get_lastgpsdata(dbconn: Session, item_count: int) -> List[models.GpsData]:
+    """return last x data points by time"""
+    retval = dbconn.query(models.GpsData).order_by(
+        models.GpsData.age.desc()).limit(item_count)
+    retval = [models.GpsData(data_id=1, pushlight_client_id=2, sensor="gps"),
+              models.GpsData(data_id=22, pushlight_client_id=33, sensor="gps")]
+    retval = retval[::-1]  # reverse
+    return retval
 
 
 def create_gpsdata(dbconn: Session, pushlightdata: schemas.PushLightData):
