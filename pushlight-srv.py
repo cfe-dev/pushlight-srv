2869f2424208f1c28b09d5a4b3b322ebacd8b7b3
cfe-dev <subcore@icb.at>
2022-11-20 22:43:55 +0100

    replace underscore w hyphen in name. RFC-1123

--- pushlight-srv.py
+++ pushlight-srv.py
@@ -17,11 +17,11 @@ from utils.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-# app = FastAPI(root_path='/pushlight_srv/v1')
+# app = FastAPI(root_path='/pushlight_srv/v1/')
 # app = FastAPI(root_path='/pushlight_srv')
 app = FastAPI()
 
-prefix_router = APIRouter(prefix="/pushlight_srv/v1")
+# route = APIRouter(prefix="/pushlight_srv/v1")
 
 
 # Dependency
@@ -38,35 +38,35 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@prefix_router.get("/", response_class=RedirectResponse)
+@app.get("/", response_class=RedirectResponse)
 def root():
     """get root html; redirect to items/1 template test"""
-    return RedirectResponse("items/1")
+    return RedirectResponse("/items/1")
 
 
-@prefix_router.get("/items/{item_id}", response_class=HTMLResponse)
-async def read_item(request: Request, item_id: str):
+@app.get("/items/{item_id}", response_class=HTMLResponse)
+async def get_items(request: Request, item_id: str):
     """get item html for specific id"""
     return templates.TemplateResponse("template_item.html",
                                       {"request": request, "item_id": item_id})
 
 
-@prefix_router.get("/items/last/{item_count}", response_class=HTMLResponse)
-async def read_lastitems(request: Request, item_count: int, dbconn: Session = Depends(get_db)):
+@app.get("/items/last/{item_count}", response_class=HTMLResponse)
+async def get_items_last(request: Request, item_count: int, dbconn: Session = Depends(get_db)):
     """get item html for specific id"""
     lastitems = crud.get_lastgpsdata(dbconn=dbconn, item_count=item_count)
     return templates.TemplateResponse("template_gpsdata.html",
                                       {"request": request, "lastitems": lastitems})
 
 
-@prefix_router.post("/collect")
+@app.post("/collect")
 async def collect(pushlightdata: schemas.PushLightData, dbconn: Session = Depends(get_db)):
     """append GPS data to persistent storage"""
     print(pushlightdata.json())
     return crud.create_gpsdata(dbconn=dbconn, pushlightdata=pushlightdata)
 
-
-app.include_router(prefix_router)
+# app.include_router(route)
 
 if __name__ == "__main__":
+    # uvicorn.run(app, host="0.0.0.0", port=8220, root_path="/pushlight_srv/v1")
     uvicorn.run(app, host="0.0.0.0", port=8220)
